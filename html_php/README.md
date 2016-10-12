efbb5cef45367d0aabc139020b9aa3bde98245b9
Monnot Stéphane <monnot.stephane@gmail.com>
2016-10-12 17:45:22 +0200

    Ajout de l'exemple PHP HTML

---
+++ html_php/README.md
@@ -0,0 +1,2 @@
+# Exemple : PHP dans HTML
+Cet exemple montre comment utiliser du code PHP à l'intérieur d'un contenu HTML
---
+++ html_php/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>HTML & PHP Examples</title>
+</head>
+
+<body>
+    <h2><?php echo strtoupper('Hello world !!!'); ?></h2>
+</body>
+
+</html>
